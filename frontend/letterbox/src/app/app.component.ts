a7ca458fddb162cc31633449589d82ebac2d3b01
Eduardo Talavera Ramos <eduardo.talavera245244@potros.itson.edu.mx>
2025-01-05 23:21:16 -0700

    Initial frontend project commit

---
+++ frontend/letterbox/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [FooterComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'letterbox';
+}
